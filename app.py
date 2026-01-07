58a38cceb75a7c9a30c066468b66dc5959b6899b
linmoxin <915673037@qq.com>
2026-01-07 20:04:02 +0800

    222

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ def hello():
     return "Hello DevOps! 自动部署测试"
 
 
-
+# git config --global http.proxy socks5://127.0.0.1:7897
+# git config --global https.proxy socks5://127.0.0.1:7897
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
